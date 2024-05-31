ca1d103ff971202e54d612789aedafb09d8cd6af
Spikree <avimahalingpure10@gmail.com>
2024-05-31 17:09:20 +0530

    added new hero images

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,45 @@
-import './Home.css'
-import heroImg from '../../assets/wati-communicate.webp'
+import "./Home.css";
+import heroImg from "../../assets/wati-communicate.webp";
+import arnav from "../../assets/arnav.jpg";
+import anvay from "../../assets/anvay.jpg";
+import { useState } from "react";
 
 const Home = () => {
-  return (
-    <div className='home'>
-        <div className="hero">
-            <div className="hero-header">
-                <h1 style={{fontSize:"50px",marginBottom:"0px"}}>Grow your business on <br /><span style={{color:"#00e785"}}>WhatsApp</span></h1>
-                <div className="hero-desc">
-                <p>
-                Personalize communication and sell more with the
-                WhatsApp Business API platform that automates
-                marketing, sales, service and support.
-                </p>
-                </div>
-            </div>
-            <div className="hero-buttons">
-                <a className='one' href="">Free 7 Day Trial</a>
-                <a className='two' href="">or book a Demo Today</a>
-            </div>
-        </div>
-        <div className="image">
-            <img src={heroImg} alt="" />
-        </div>
-    </div>
-  )
-}
+  const [heroImage, setHeroImage] = useState(anvay);
 
-export default Home
+  return (
+    <div className="home">
+      <div className="hero">
+        <div className="hero-header">
+          <h1 style={{ fontSize: "50px", marginBottom: "0px" }}>
+            Grow your business on <br />
+            <span style={{ color: "#00e785" }}>WhatsApp</span>
+          </h1>
+          <div className="hero-desc">
+            <p>
+              Personalize communication and sell more with the WhatsApp Business
+              API platform that automates marketing, sales, service and support.
+            </p>
+          </div>
+        </div>
+        <div className="hero-buttons">
+          <a className="one" href="">
+            Free 7 Day Trial
+          </a>
+          <a className="two" href="">
+            or book a Demo Today
+          </a>
+        </div>
+      </div>
+      <div
+        onMouseEnter={() => setHeroImage(arnav)}
+        onMouseLeave={() => setHeroImage(anvay)}
+        className="image"
+      >
+        <img src={heroImage} alt="Hero" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
